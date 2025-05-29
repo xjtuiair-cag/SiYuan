737a8c92b7f8e03fbbdac827b4566bf6749c483a
2002liush <liushenghuan2002@gmail.com>
2025-05-29 12:01:25 +0000

    more clk source

--- fpga/xilinx_ip/xlnx_clk_gen/tcl/run.tcl
+++ fpga/xilinx_ip/xlnx_clk_gen/tcl/run.tcl
@@ -9,8 +9,13 @@ set_property board_part $boardName [current_project]
 create_ip -name clk_wiz -vendor xilinx.com -library ip -module_name $ipName
 
 set_property -dict [list CONFIG.PRIM_IN_FREQ {200.000} \
-                        CONFIG.NUM_OUT_CLKS {1} \
+                        CONFIG.NUM_OUT_CLKS {3} \
+                        CONFIG.CLKOUT2_USED {true} \
+                        CONFIG.CLKOUT3_USED {true} \
                         CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {50} \
+                        CONFIG.CLKOUT2_REQUESTED_OUT_FREQ {125} \
+                        CONFIG.CLKOUT3_REQUESTED_OUT_FREQ {125} \
+                        CONFIG.CLKOUT3_REQUESTED_PHASE {90.000} \
                         CONFIG.CLKIN1_JITTER_PS {50.0} \
                        ] [get_ips $ipName]
 
